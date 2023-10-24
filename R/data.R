1c943724e2f72a09a88cffebc23d75816147b086
Dilancifci <s233597@dtu.dk>
2023-10-24 10:32:49 +0200

    Hej

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon table
+#'
+#' The table gives us an overview of all the codons, and the amino acid residues that match the codons.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"Codon_table"
---
+++ data-raw/Codon_table.R
@@ -0,0 +1,3 @@
+## code to prepare `Codon_table` dataset goes here
+
+usethis::use_data(Codon_table, overwrite = TRUE)
